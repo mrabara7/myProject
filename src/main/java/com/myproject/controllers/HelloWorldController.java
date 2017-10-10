14ea8ed10274e74f58e1d03ca3abc1844a3c04da
mrabara7 <mrabara7@gmail.com>
2017-10-10 17:59:13 +0800

    Added bootstrap and jquery in pom

--- src/main/java/com/myproject/controllers/HelloWorldController.java
+++ src/main/java/com/myproject/controllers/HelloWorldController.java
@@ -1,7 +1,15 @@
 package com.myproject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by DICT on 10/10/2017.
  */
+@Controller
 public class HelloWorldController {
+    @RequestMapping("/")
+    public String sayHello(){
+        return "index";
+    }
 }
