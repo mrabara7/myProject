32658e406cfb46d5b95b5a2e0fd4687f33db7c78
mrabara7 <mrabara7@gmail.com>
2017-10-11 14:09:23 +0800

    Added placeholder

---
+++ src/main/java/com/myproject/web/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.myproject.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by DICT on 11/10/2017.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping
+    public String home(){
+
+        return "index";
+    }
+}
